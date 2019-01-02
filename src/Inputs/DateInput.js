85a0e3310aea9def301a6b69d3cc58db5142bd4d
Nikolay Slavov <nikolay@slavov.org>
2019-01-02 16:38:50 +0200

    Comment date input fields

--- src/Inputs/DateInput.js
+++ src/Inputs/DateInput.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { SingleDatePicker } from 'react-dates';
-import moment from 'moment';
-import { compose, withStateHandlers } from 'recompose';
+// import React from 'react';
+// import 'react-dates/initialize';
+// import 'react-dates/lib/css/_datepicker.css';
+// import { SingleDatePicker } from 'react-dates';
+// import moment from 'moment';
+// import { compose, withStateHandlers } from 'recompose';
 
-const DateInput = ({ name, focuseHandler, focused, formCtx: { values, setFieldValue }, ...props }) => (
-  <SingleDatePicker
-    date={moment(values[name])}
-    onDateChange={date => setFieldValue(name, date.toISOString())}
-    focused={focused}
-    onFocusChange={({ focused }) => focuseHandler(focused)}
-    id={`${name}_date_picker`}
-    {...props}
-  />
-);
+// const DateInput = ({ name, focuseHandler, focused, formCtx: { values, setFieldValue }, ...props }) => (
+//   <SingleDatePicker
+//     date={moment(values[name])}
+//     onDateChange={date => setFieldValue(name, date.toISOString())}
+//     focused={focused}
+//     onFocusChange={({ focused }) => focuseHandler(focused)}
+//     id={`${name}_date_picker`}
+//     {...props}
+//   />
+// );
 
-const withFocusHandlers = withStateHandlers(
-  { focused: false },
-  { focuseHandler: () => (focused) => ({ focused }) }
-);
+// const withFocusHandlers = withStateHandlers(
+//   { focused: false },
+//   { focuseHandler: () => (focused) => ({ focused }) }
+// );
 
-export default compose(
-  withFocusHandlers
-)(DateInput);
+// export default compose(
+//   withFocusHandlers
+// )(DateInput);
+
+export default () => null;
--- src/Inputs/DateRangeInput.js
+++ src/Inputs/DateRangeInput.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { DateRangePicker } from 'react-dates';
-import moment from 'moment';
-import { compose, withStateHandlers } from 'recompose';
+// import React from 'react';
+// import 'react-dates/initialize';
+// import 'react-dates/lib/css/_datepicker.css';
+// import { DateRangePicker } from 'react-dates';
+// import moment from 'moment';
+// import { compose, withStateHandlers } from 'recompose';
 
-const DateRangeInput = ({
-  startFieldName,
-  endFieldName,
-  focuseHandler,
-  focused,
-  formCtx: { values, setFieldValue },
-  name,
-  placeholder,
-  ...props
-}) => {
-  const startDateValue = values[startFieldName];
-  const endDateValue = values[endFieldName];
+// const DateRangeInput = ({
+//   startFieldName,
+//   endFieldName,
+//   focuseHandler,
+//   focused,
+//   formCtx: { values, setFieldValue },
+//   name,
+//   placeholder,
+//   ...props
+// }) => {
+//   const startDateValue = values[startFieldName];
+//   const endDateValue = values[endFieldName];
 
-  return (
-    <DateRangePicker
-      startDate={moment(startDateValue)}
-      startDateId={`${startFieldName}_date_range_picker`}
-      endDate={moment(endDateValue)}
-      endDateId={`${endFieldName}_date_range_picker`}
-      onDatesChange={({ startDate, endDate }) => {
-        if (startDate) setFieldValue(startFieldName, startDate.toISOString());
-        if (endDate) setFieldValue(endFieldName, endDate.toISOString());
-      }}
-      focusedInput={focused}
-      onFocusChange={(focusedInput) => focuseHandler(focusedInput)}
-      {...props}
-    />
-  );
-}
+//   return (
+//     <DateRangePicker
+//       startDate={moment(startDateValue)}
+//       startDateId={`${startFieldName}_date_range_picker`}
+//       endDate={moment(endDateValue)}
+//       endDateId={`${endFieldName}_date_range_picker`}
+//       onDatesChange={({ startDate, endDate }) => {
+//         if (startDate) setFieldValue(startFieldName, startDate.toISOString());
+//         if (endDate) setFieldValue(endFieldName, endDate.toISOString());
+//       }}
+//       focusedInput={focused}
+//       onFocusChange={(focusedInput) => focuseHandler(focusedInput)}
+//       {...props}
+//     />
+//   );
+// }
 
-const withFocusHandlers = withStateHandlers(
-  { focused: null },
-  { focuseHandler: () => (focused) => ({ focused }) }
-);
+// const withFocusHandlers = withStateHandlers(
+//   { focused: null },
+//   { focuseHandler: () => (focused) => ({ focused }) }
+// );
 
-export default compose(
-  withFocusHandlers
-)(DateRangeInput);
+// export default compose(
+//   withFocusHandlers
+// )(DateRangeInput);
+export default () => null;
